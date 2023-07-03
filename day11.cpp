9ea808e483706338d62ae4a28490bbea2e6afd08
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2023-07-03 19:11:02 +0530

    first commit

---
+++ day11.cpp
@@ -0,0 +1,85 @@
+// 11. Remove Nth Node From End of List
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+
+        for (int i = 0; i <= n; ++i) {
+            fast = fast->next;
+        }
+
+        while (fast) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        ListNode* toDelete = slow->next;
+        slow->next = slow->next->next;
+        delete toDelete;
+
+        ListNode* newHead = dummy->next;
+        delete dummy;
+
+        return newHead;
+    }
+};
+
+// Helper function to create a linked list from an array
+ListNode* createLinkedList(int arr[], int size) {
+    if (size == 0) return nullptr;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* current = head;
+    for (int i = 1; i < size; ++i) {
+        current->next = new ListNode(arr[i]);
+        current = current->next;
+    }
+    return head;
+}
+
+// Helper function to print a linked list
+void printLinkedList(ListNode* head) {
+    ListNode* current = head;
+    while (current) {
+        std::cout << current->val << " -> ";
+        current = current->next;
+    }
+    std::cout << "nullptr" << std::endl;
+}
+
+int main() {
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int n = 2;
+
+    ListNode* head = createLinkedList(arr, sizeof(arr) / sizeof(arr[0]));
+    std::cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    Solution sol;
+    ListNode* newHead = sol.removeNthFromEnd(head, n);
+
+    std::cout << "Linked List after removal: ";
+    printLinkedList(newHead);
+
+    // Free memory to avoid memory leaks
+    while (newHead) {
+        ListNode* temp = newHead;
+        newHead = newHead->next;
+        delete temp;
+    }
+
+    return 0;
+}
