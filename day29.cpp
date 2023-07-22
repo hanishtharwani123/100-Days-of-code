abbbe09b86ce0a51529d7f179a1a31a9746d5a42
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2023-07-22 19:19:49 +0530

    first commit

---
+++ day29.cpp
@@ -0,0 +1,48 @@
+// 29. Group Anagrams
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // Create a hash map to store the anagrams
+        std::unordered_map<std::string, std::vector<std::string>> anagramMap;
+
+        // Iterate through each word in the input array
+        for (const std::string& word : strs) {
+            // Sort the word to find its anagram representation
+            std::string sortedWord = word;
+            std::sort(sortedWord.begin(), sortedWord.end());
+
+            // Add the word to its corresponding anagram group in the hash map
+            anagramMap[sortedWord].push_back(word);
+        }
+
+        // Convert the hash map values to the final result vector
+        std::vector<std::vector<std::string>> result;
+        for (const auto& pair : anagramMap) {
+            result.push_back(pair.second);
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::vector<std::string> strs = {"eat","tea","tan","ate","nat","bat"};
+    std::vector<std::vector<std::string>> result = solution.groupAnagrams(strs);
+
+    // Print the output
+    for (const auto& group : result) {
+        for (const std::string& word : group) {
+            std::cout << word << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
