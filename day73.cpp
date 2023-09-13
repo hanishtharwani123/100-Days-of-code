262ca0a13bece725a59c82e07c8d1e8e2abd373b
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2023-09-13 23:29:11 +0530

    first commit

---
+++ day73.cpp
@@ -0,0 +1,63 @@
+// 73. Number of Islands
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty() || grid[0].empty()) {
+            return 0;
+        }
+
+        int numIslands = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '1') {
+                    ++numIslands;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return numIslands;
+    }
+
+private:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0'; // Mark the current land as visited
+
+        // Visit all adjacent lands
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+};
+
+int main() {
+    vector<vector<char>> grid = {
+        {'1', '1', '1', '1', '0'},
+        {'1', '1', '0', '1', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'0', '0', '0', '0', '0'}
+    };
+
+    Solution solution;
+    int result = solution.numIslands(grid);
+
+    cout << "Number of Islands: " << result << endl;
+
+    return 0;
+}
