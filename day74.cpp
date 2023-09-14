05d276e672bbdbc571e0c1ebfc6d896e1f46d9e2
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2023-09-14 23:35:54 +0530

    first commit

---
+++ day74.cpp
@@ -0,0 +1,58 @@
+// 74. Reverse Linked List
+#include <iostream>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* current = head;
+        ListNode* next = nullptr;
+
+        while (current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+};
+
+// Function to print a linked list.
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        std::cout << current->val << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    std::cout << "Original list: ";
+    printList(head);
+
+    Solution solution;
+    // Reverse the linked list
+    head = solution.reverseList(head);
+
+    std::cout << "Reversed list: ";
+    printList(head);
+
+    return 0;
+}
